1a0133ffdc52f314b2115c886fb80fd6ed1934dd
Christopher Peplin <peplin@bueda.com>
2011-04-30 18:45:34 -0400

    Run 1 celery task per notice queue batch, now safe for multiple celery workers.
    Conflicts:
    
    	notification/engine.py
    	notification/models.py

--- notification/models.py
+++ notification/models.py
@@ -365,11 +365,14 @@ def queue(users, label, extra_context=None, on_site=True, sender=None):
     notices = []
     for user in users:
         notices.append((user, label, extra_context, on_site, sender))
-    NoticeQueueBatch(pickled_data=pickle.dumps(notices).encode("base64")).save()
-
+    batch = NoticeQueueBatch(pickled_data=pickle.dumps(notices).encode(
+            "base64"))
+    batch.save()
     if 'djcelery' in settings.INSTALLED_APPS:
-        from notification.tasks import emit_notices
-        emit_notices.delay()
+        # TODO could also send a task per notice and drop the whole batch
+        # thing
+        from notification.tasks import emit_notice_batch
+        emit_notice_batch.delay(batch.id)
 
 class ObservedItemManager(models.Manager):
 
--- notification/tasks.py
+++ notification/tasks.py
@@ -1,12 +1,10 @@
 from celery.decorators import task
 
-from notification.engine import send_all
+from notification.engine import emit_batch
+from notification.models import NoticeQueueBatch
 
 
-@task(ignore_result=True, rate_limit='6/m')
-def emit_notices():
-    """Don't fire this too often if using multiple celery workers - the send_all
-    engine is not safe to use cross-machine, since the lock it uses is on a
-    local disk.
-    """
-    send_all()
+@task(ignore_result=True)
+def emit_notice_batch(notice_batch_id, **kwargs):
+    batch = NoticeQueueBatch.objects.get(id=notice_batch_id)
+    emit_batch(batch)
