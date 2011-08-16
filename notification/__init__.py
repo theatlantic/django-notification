be46401aea796d6b43644d6c8915afb170984208
Frankie Dintino <fdintino@theatlantic.com>
2011-08-16 16:29:12 -0400

    Modified __init__.py to get rid of awkward "from notification import models as notification"

--- notification/__init__.py
+++ notification/__init__.py
@@ -1,71 +1,8 @@
-from django.utils import importlib
-from django.core.exceptions import ImproperlyConfigured
-
 VERSION = (0, 2, 0, "a", 1) # following PEP 386
 DEV_N = 4
+__version__ = '0.2a1.dev4'
 
-def get_version():
-    version = "%s.%s" % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = "%s.%s" % (version, VERSION[2])
-    if VERSION[3] != "f":
-        version = "%s%s%s" % (version, VERSION[3], VERSION[4])
-        if DEV_N:
-            version = "%s.dev%s" % (version, DEV_N)
-    return version
-
-__version__ = get_version()
-
-# Name for use in settings file --> name of module in "backends" directory.
-# Any backend scheme that is not in this dictionary is treated as a Python
-# import path to a custom backend.
-BACKENDS = {
-    'email': 'email.EmailBackend',
-    'facebook': 'fb.FacebookWallPostBackend',
-    'web': 'web.WebBackend',
-    'dummy': 'dummy.DummyBackend',
-}
-
-DEFAULT_BACKENDS = ['email', 'web']
-
-def load_backend(backend):
-    if backend in BACKENDS:
-        path = 'notification.backends.%s' % BACKENDS[backend]
-    else:
-        path = backend
-
-    i = path.rfind('.')
-    module, attr = path[:i], path[i+1:]
-    try:
-        mod = importlib.import_module(module)
-    except ImportError, e:
-        raise ImproperlyConfigured('Error importing notification backend %s: "%s"' % (module, e))
-    except ValueError, e:
-        raise ImproperlyConfigured('Error importing notification backends. Is NOTIFICATION_BACKENDS a correctly defined list or tuple?')
-    try:
-        cls = getattr(mod, attr)
-    except AttributeError:
-        raise ImproperlyConfigured('Module "%s" does not define a "%s" notification backend' % (module, attr))
-
-    return cls()
-
-def get_backends():
-    from django.conf import settings
-    backends = []
-    for backend in getattr(settings, 'NOTIFICATION_BACKENDS', []):
-        backends.append(load_backend(backend))
-    if not backends:
-        for backend in DEFAULT_BACKENDS:
-            backends.append(load_backend(backend))
-    return set(backends)
-
-backends = get_backends()
-
-def get_backend_field_choices():
-    choices = []
-    for backend in backends:
-        name = "%s.%s" % (backend.__module__, backend.__class__.__name__)
-        choices.append((name, backend.display_name))
-    return set(choices)
-
-backend_field_choices = get_backend_field_choices()
+from notification.backends import backends, backend_field_choices
+from notification.models import create_notice_type, get_notification_language, \
+	send_now, send, queue, observe, stop_observing, send_observation_notices_for, \
+	is_observing, handle_observations
