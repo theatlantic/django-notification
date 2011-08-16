53ed24e725b657188906e9f52afd13b897e0f413
Frankie Dintino <fdintino@theatlantic.com>
2011-08-16 19:16:52 -0400

    Same issue as previous commit, wrapping import of notification.models in __init__.py

--- notification/__init__.py
+++ notification/__init__.py
@@ -2,6 +2,9 @@ VERSION = (0, 2, 0, "a", 1) # following PEP 386
 DEV_N = 4
 __version__ = '0.2a1.dev4'
 
-from notification.models import create_notice_type, get_notification_language, \
-	send_now, send, queue, observe, stop_observing, send_observation_notices_for, \
-	is_observing, handle_observations
+try:
+    from notification.models import create_notice_type, get_notification_language, \
+    	send_now, send, queue, observe, stop_observing, send_observation_notices_for, \
+    	is_observing, handle_observations
+except ImportError:
+    pass
